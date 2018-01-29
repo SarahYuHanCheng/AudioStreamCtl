a18ee4a6c5465c0e866de8b1073b7db53cd06740
SarahYuHanCheng <bigbigtreeful@gmail.com>
2018-01-29 12:15:19 +0800

    add vol

--- RpiServer.js
+++ RpiServer.js
@@ -16,18 +16,26 @@ server = http.createServer( function(req, res) {
 
             var obj_body = body.slice(2,10);//__UserID 8, SongName 8, PlaySong 8
            
-            if(obj_body=='PlaySong'){
-
-                var cmdString = 'firefox https://www.youtube.com/watch?v='+par_data.url+'?autoplay=1';
-                console.log(cmdString);
-                child(cmdString, (err, stdout, stderr) => {
-                  // var the_res = stdout.split("\n",2);
-                  // res.end(the_res[1]);
+            if(obj_body=='PlayerEq'){
+            	var PlayerEq_rec = par_data.PlayerEq;
+            	console.log(PlayerEq_rec);
+            	console.log(par_data.PlayerEq[0]);
+            	
+                // // var cmdString = 'firefox https://www.youtube.com/watch?v='+par_data.url+'?autoplay=1';
+                // var cmdString = 'aplay -D equal '+par_data.url+' &';
+                // // for (var i = Things.length - 1; i >= 0; i--) {
+                // // 	cmdString+='amixer -D equal cset numid='+numid+' '+gain;
+                // // }
+                // 'amixer -D equal cset numid='+numid+' '+gain
+                // console.log(cmdString);
+                // child(cmdString, (err, stdout, stderr) => {
+                //   // var the_res = stdout.split("\n",2);
+                //   // res.end(the_res[1]);
                   
-                });
-            }else if(obj_body=='PlaySong_fromserver'){
-
-                var cmdString = 'firefox test.html';
+                // });
+            }else if(obj_body=='Play_vol'){
+            	// var vol_rec = par_data.Play_vol;
+                var cmdString = 'amixer --card=0 sset Digital '+par_data.Play_vol+'%';
                 console.log(cmdString);
                 child(cmdString, (err, stdout, stderr) => {
                   // var the_res = stdout.split("\n",2);
